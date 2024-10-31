3084a8a3c541b2150ce0abf1d486559fcee1a472
Jacob Clark <111930333+JClark-32@users.noreply.github.com>
2024-10-31 14:19:58 +0000

    initial files

---
+++ test/test.js
@@ -0,0 +1,14 @@
+var main = require('../index');
+var assert = require('assert');
+describe('Regular factorial value', function() {
+    it('should return 120 for Fact(5)', function() {
+        assert.strictEqual(main.factorial(5), 120);
+      });
+});
+
+describe('First two terms', function() {
+  it('should return 1 for first two terms', function() {
+      assert.strictEqual(main.factorial(0), 1);
+      assert.strictEqual(main.factorial(1), 1);
+    });
+});
